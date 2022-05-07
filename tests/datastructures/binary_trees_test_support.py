3d3c627c9f45f5efd746e2fbbe0cc8d03790f8d0
Mario Scalas <mario.scalas@gmail.com>
2022-05-07 20:59:18 +0100

    Removed the binary tree class since it is useless.

--- tests/datastructures/binary_trees_test_support.py
+++ tests/datastructures/binary_trees_test_support.py
@@ -1,25 +1,26 @@
 """Test support for binary trees."""
-from interviews.datastructures.binary_trees import BinaryTree
+from __future__ import annotations
+
+from typing import Optional
 from interviews.datastructures.binary_trees import Node
 
 TestNode = Node[str, str]
-TestBinaryTree = BinaryTree[str, str]
 
 
-def _create_empty_binary_tree() -> TestBinaryTree:
-    return TestBinaryTree()
+def _create_empty_binary_tree() -> (TestNode | None):
+    return None
 
 
-def _create_single_node_binary_tree() -> TestBinaryTree:
-    return TestBinaryTree(TestNode("root", "root_value"))
+def _create_single_node_binary_tree() -> TestNode:
+    return TestNode("root", "root_value", None, None)
 
 
-def _create_3_nodes_balanced_binary_tree() -> TestBinaryTree:
+def _create_3_nodes_balanced_binary_tree() -> TestNode:
     root = TestNode("root", "root_value")
     root.left_child = TestNode("left", "left_value")
     root.right_child = TestNode("right", "right_value")
 
-    return TestBinaryTree(root)
+    return root
 
 
 EMPTY_BINARY_TREE = _create_empty_binary_tree()
--- tests/datastructures/test_binary_trees.py
+++ tests/datastructures/test_binary_trees.py
@@ -2,20 +2,18 @@
 from typing import Callable
 from typing import List
 from typing import Optional
-from unittest.mock import Mock
 
 import pytest
-from pytest_mock import MockerFixture
 
 from .binary_trees_test_support import EMPTY_BINARY_TREE
 from .binary_trees_test_support import SINGLE_NODE_BINARY_TREE
-from .binary_trees_test_support import TestBinaryTree
 from .binary_trees_test_support import TestNode
 from .binary_trees_test_support import THREE_NODES_BINARY_TREE
-from interviews.datastructures.binary_trees import _visit_in_order
-from interviews.datastructures.binary_trees import _visit_post_order
-from interviews.datastructures.binary_trees import _visit_pre_order
 from interviews.datastructures.binary_trees import ActionCallback
+from interviews.datastructures.binary_trees import is_empty
+from interviews.datastructures.binary_trees import visit_in_order
+from interviews.datastructures.binary_trees import visit_post_order
+from interviews.datastructures.binary_trees import visit_pre_order
 
 test_create_node_data = [
     (TestNode("k1", "v1"), "k1", "v1"),
@@ -52,24 +50,24 @@ test_binary_tree_is_empty_data = [
 
 
 @pytest.mark.parametrize("tree,expected_is_empty", test_binary_tree_is_empty_data)
-def test_binary_tree_is_empty(tree: TestBinaryTree, expected_is_empty: bool) -> None:
+def test_binary_tree_is_empty(tree: TestNode | None, expected_is_empty: bool) -> None:
     """Test that is_empty() is correct."""
-    assert tree.is_empty() == expected_is_empty
+    assert is_empty(tree) == expected_is_empty
 
 
 # Just a shortcut for improving readability
 VisitFunctionType = Callable[[Optional[TestNode], ActionCallback[str, str]], None]
 
 test_in_order_visit_data = [
-    (_visit_in_order, EMPTY_BINARY_TREE.root, []),
-    (_visit_in_order, SINGLE_NODE_BINARY_TREE.root, ["root"]),
-    (_visit_in_order, THREE_NODES_BINARY_TREE.root, ["left", "root", "right"]),
-    (_visit_pre_order, EMPTY_BINARY_TREE.root, []),
-    (_visit_pre_order, SINGLE_NODE_BINARY_TREE.root, ["root"]),
-    (_visit_pre_order, THREE_NODES_BINARY_TREE.root, ["root", "left", "right"]),
-    (_visit_post_order, EMPTY_BINARY_TREE.root, []),
-    (_visit_post_order, SINGLE_NODE_BINARY_TREE.root, ["root"]),
-    (_visit_post_order, THREE_NODES_BINARY_TREE.root, ["left", "right", "root"]),
+    (visit_in_order, EMPTY_BINARY_TREE, []),
+    (visit_in_order, SINGLE_NODE_BINARY_TREE, ["root"]),
+    (visit_in_order, THREE_NODES_BINARY_TREE, ["left", "root", "right"]),
+    (visit_pre_order, EMPTY_BINARY_TREE, []),
+    (visit_pre_order, SINGLE_NODE_BINARY_TREE, ["root"]),
+    (visit_pre_order, THREE_NODES_BINARY_TREE, ["root", "left", "right"]),
+    (visit_post_order, EMPTY_BINARY_TREE, []),
+    (visit_post_order, SINGLE_NODE_BINARY_TREE, ["root"]),
+    (visit_post_order, THREE_NODES_BINARY_TREE, ["left", "right", "root"]),
 ]
 
 
@@ -78,7 +76,7 @@ test_in_order_visit_data = [
 )
 def test_inorder_visit(
     visit_function: VisitFunctionType,
-    tree_root_node: TestNode,
+    tree_root_node: TestNode | None,
     expected_visited_items: List[str],
 ) -> None:
     """Ensure that we can visit the nodes and its child correctly."""
@@ -91,98 +89,3 @@ def test_inorder_visit(
     visit_function(tree_root_node, collect)
 
     assert collected_nodes == expected_visited_items
-
-
-test_binary_tree_inorder_visit_data = [
-    (EMPTY_BINARY_TREE, 1),
-    (SINGLE_NODE_BINARY_TREE, 1),
-    (THREE_NODES_BINARY_TREE, 1),
-]
-
-
-# The _visit_in_order() and similar functions are mocked in these tests.
-# Yet, we want to be sure to call it once to just start visiting the tree.
-
-
-@pytest.fixture
-def mock_visit_in_order(
-    mocker: MockerFixture,
-) -> Mock:
-    """Provides a mock for the _visit_in_order() function."""
-    return mocker.patch(
-        "interviews.datastructures.binary_trees._visit_in_order", return_value=None
-    )
-
-
-@pytest.mark.parametrize(
-    "tree,expected_num_visit_calls", test_binary_tree_inorder_visit_data
-)
-def test_binary_tree_in_order_visit(
-    tree: TestBinaryTree,
-    expected_num_visit_calls: int,
-    mock_visit_in_order: Mock,
-) -> None:
-    """Verify that we call the _visit_in_order function."""
-
-    def do_nothing(_: TestNode) -> bool:
-        pass
-
-    tree.visit_in_order(do_nothing)
-
-    assert mock_visit_in_order.call_count == expected_num_visit_calls
-
-
-@pytest.fixture
-def mock_visit_pre_order(
-    mocker: MockerFixture,
-) -> Mock:
-    """Provides a mock for the _visit_pre_order() function."""
-    return mocker.patch(
-        "interviews.datastructures.binary_trees._visit_pre_order", return_value=None
-    )
-
-
-@pytest.mark.parametrize(
-    "tree,expected_num_visit_calls", test_binary_tree_inorder_visit_data
-)
-def test_binary_tree_pre_order_visit(
-    tree: TestBinaryTree,
-    expected_num_visit_calls: int,
-    mock_visit_pre_order: Mock,
-) -> None:
-    """Verify that we call the mock_visit_pre_order function."""
-
-    def do_nothing(_: TestNode) -> bool:
-        pass
-
-    tree.visit_pre_order(do_nothing)
-
-    assert mock_visit_pre_order.call_count == expected_num_visit_calls
-
-
-@pytest.fixture
-def mock_visit_post_order(
-    mocker: MockerFixture,
-) -> Mock:
-    """Provides a mock for the _visit_post_order() function."""
-    return mocker.patch(
-        "interviews.datastructures.binary_trees._visit_post_order", return_value=None
-    )
-
-
-@pytest.mark.parametrize(
-    "tree,expected_num_visit_calls", test_binary_tree_inorder_visit_data
-)
-def test_binary_tree_post_order_visit(
-    tree: TestBinaryTree,
-    expected_num_visit_calls: int,
-    mock_visit_post_order: Mock,
-) -> None:
-    """Verify that we call the mock_visit_post_order function."""
-
-    def do_nothing(_: TestNode) -> bool:
-        pass
-
-    tree.visit_post_order(do_nothing)
-
-    assert mock_visit_post_order.call_count == expected_num_visit_calls
